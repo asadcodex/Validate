cba6968a2cd40e87c30adfcd37db01beb5ceaa5e
asadcodex <aslanrazasr@gmail.com>
2025-04-03 22:35:10 +0530

    sort method

--- calculator/script.js
+++ calculator/script.js
@@ -1,4 +1,6 @@
-const getMean = (array) => {
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
+
+const getMedian = (array) => {
 
 }
 
@@ -6,4 +8,8 @@ const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+  
+  const mean = getMean(numbers);
+
+  document.querySelector("#mean").textContent = mean;
 }
