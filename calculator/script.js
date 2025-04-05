12972ac63eeeaad5b25b48d00c6707ea00b4b4e3
asadcodex <aslanrazasr@gmail.com>
2025-04-05 23:04:32 +0530

    calc 30

--- calculator/script.js
+++ calculator/script.js
@@ -1,8 +1,18 @@
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
 const getMedian = (array) => {
+  const sorted = array.toSorted((a, b) => a - b);
+  const median =
+    sorted.length % 2 === 0
+      ? getMean([sorted[sorted.length / 2], sorted[sorted.length / 2 - 1]])
+      : sorted[Math.floor(sorted.length / 2)];
+  return median;
+}
+
+const getMode = (array) => {
 
 }
+
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
@@ -10,6 +20,8 @@ const calculate = () => {
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
   
   const mean = getMean(numbers);
+  const median = getMedian(numbers);
 
   document.querySelector("#mean").textContent = mean;
+  document.querySelector("#median").textContent = median;
 }
