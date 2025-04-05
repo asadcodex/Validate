cdfe1e2a9c96686e052df810c3f7cb38396fc7ee
asadcodex <aslanrazasr@gmail.com>
2025-04-05 23:05:12 +0530

    cal 40

--- calculator/script.js
+++ calculator/script.js
@@ -10,7 +10,14 @@ const getMedian = (array) => {
 }
 
 const getMode = (array) => {
-
+  const counts = {};
+  array.forEach((el) => {
+    counts[el] = (counts[el] || 0) + 1;
+  })
+  if (new Set(Object.values(counts)).size === 1) {
+    return null;
+  }
+  const highest = Object.keys(counts);
 }
 
 
