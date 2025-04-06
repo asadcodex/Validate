cdf87537361c7e7b38a5238fa9e9ce999b4bc510
asadcodex <aslanrazasr@gmail.com>
2025-04-06 23:35:26 +0530

    squared differences sum 50

--- calculator/script.js
+++ calculator/script.js
@@ -17,9 +17,29 @@ const getMode = (array) => {
   if (new Set(Object.values(counts)).size === 1) {
     return null;
   }
-  const highest = Object.keys(counts);
+  const highest = Object.keys(counts).sort(
+    (a, b) => counts[b] - counts[a]
+  )[0];
+  const mode = Object.keys(counts).filter(
+    (el) => counts[el] === counts[highest]
+  );
+  return mode.join(", ");
 }
 
+const getRange = (array) => {
+  return Math.max(...array) - Math.min(...array);
+}
+
+const getVariance = (array) => {
+  const mean = getMean(array);
+  const differences = array.map(
+    el => el - mean
+  );
+  const squaredDifferences = differences.map(
+    el => el ** 2
+  );
+
+}
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
@@ -28,7 +48,11 @@ const calculate = () => {
   
   const mean = getMean(numbers);
   const median = getMedian(numbers);
+  const mode = getMode(numbers);
+  const range = getRange(numbers);
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
+  document.querySelector("#mode").textContent = mode;
+  document.querySelector("#range").textContent = range;
 }
